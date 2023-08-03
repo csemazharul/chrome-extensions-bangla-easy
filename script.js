39c338163ebd762c734c2b158fd864780d949821
Md Mazharul Islam <54615735+csemazharul@users.noreply.github.com>
2023-08-04 00:45:31 +0600

    feat: deleted script js file

--- script.js
+++
@@ -1,8 +0,0 @@
-const btn=document.getElementById("txt")
-
-btn.addEventListener("dblclick", event=>{
-    const selectedText=window.getSelection().toString()
-    if(selectedText.length>0){
-        console.log(selectedText, 'selected')
-    }
-})
