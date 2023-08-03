81365da747080496f2d1c5b346031a4e0e51f074
Md Mazharul Islam <54615735+csemazharul@users.noreply.github.com>
2023-08-04 00:45:43 +0600

    feat: added script file

--- script/script.js
+++ script/script.js
@@ -1,8 +1,25 @@
-const btn=document.getElementById("txt")
-
-btn.addEventListener("dblclick", event=>{
-    const selectedText=window.getSelection().toString()
-    if(selectedText.length>0){
-        console.log(selectedText, 'selected')
-    }
-})
+chrome.contextMenus.create({
+    title: "Translate to Bangla",
+    contexts: ["selection"],
+    onclick: translateSelection
+  });
+  
+  function translateSelection(info, tab) {
+    const selectedText = info.selectionText;
+    const translatedText = translateToBangla(selectedText);
+    showTranslationPopup(translatedText);
+  }
+  
+  function translateToBangla(text) {
+    return text;
+  }
+  
+  function showTranslationPopup(text) {
+    chrome.notifications.create({
+      type: "basic",
+      iconUrl: "icon.png", 
+      title: "Translation",
+      message: text
+    });
+  }
+  
